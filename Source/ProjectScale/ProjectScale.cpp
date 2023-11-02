b3fb2f98d416c2a8ba94433d3d66dde3ec1c4c17
jacobtydrake <jacobtydrake@gmail.com>
2023-11-02 17:48:15 -0500

    project creation

---
+++ Source/ProjectScale/ProjectScale.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectScale.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectScale, "ProjectScale" );
