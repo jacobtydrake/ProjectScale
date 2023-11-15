6b7267e3923bed5e42b1ecd5548f1f7299f4da70
jacobtydrake <jacobtydrake@gmail.com>
2023-11-14 22:54:01 -0600

    Enemy Update
    - Default Enemies can:
    -- move
    -- take damage
    -- die

--- Source/ProjectScale/Actors/PSEnemy.cpp
+++ Source/ProjectScale/Actors/PSEnemy.cpp
@@ -2,6 +2,10 @@
 #include "PSEnemy.h"
 #include "Components/CapsuleComponent.h"
 #include "PaperFlipbookComponent.h"
+#include "Components/ArrowComponent.h"
+#include "Particles/ParticleSystemComponent.h"
+
+DEFINE_LOG_CATEGORY(PSEnemy);
 
 APSEnemy::APSEnemy()
 {
@@ -10,7 +14,6 @@ APSEnemy::APSEnemy()
 	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComponent"));
 	RootComponent = CapsuleComp;
 
-	// Initialize and set up the Flipbook Component
 	FlipbookComp = CreateDefaultSubobject<UPaperFlipbookComponent>(TEXT("FlipbookComponent"));
 	FlipbookComp->SetupAttachment(RootComponent);
 	FlipbookComp->bOwnerNoSee = false;
@@ -18,6 +21,20 @@ APSEnemy::APSEnemy()
 	FlipbookComp->PrimaryComponentTick.TickGroup = TG_PrePhysics;
 	FlipbookComp->SetCollisionProfileName(TEXT("DefaultEnemyCollision"));
 	FlipbookComp->SetGenerateOverlapEvents(true);
+
+	DirectionArrow = CreateDefaultSubobject<UArrowComponent>(TEXT("DirectionArrow"));
+	DirectionArrow->SetupAttachment(RootComponent);
+	DirectionArrow->ArrowSize = 2.0f;
+
+	DeathEffect = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("DeathEffect"));
+	DeathEffect->SetupAttachment(RootComponent);
+	DeathEffect->bAutoActivate = false;
+}
+
+APSEnemy::APSEnemy(FVector NewDirection)
+	: MovementDirection(NewDirection)
+{
+	//do other stuff ig
 }
 
 void APSEnemy::BeginPlay()
@@ -29,17 +46,49 @@ void APSEnemy::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (bShouldMove)
+	{
+		FVector Direction = DirectionArrow->GetForwardVector();
+		FVector NewLocation = GetActorLocation() + (Direction * MovementSpeed * DeltaTime);
+		SetActorLocation(NewLocation);
+	}
 }
 
 void APSEnemy::TakeDamage_Implementation(const float DamageAmount)
 {
 	UE_LOG(LogTemp, Display, TEXT("TakeDamage_Implementation: %f"), DamageAmount);
 
+	FlipbookComp->SetSpriteColor(FLinearColor::Red);
 
 	FTimerHandle TimerHandle;
 	GetWorldTimerManager().SetTimer(TimerHandle, this, &APSEnemy::RevertSpriteColor, 0.1f, false);
 
-	FlipbookComp->SetSpriteColor(FLinearColor::Red);
+	CurrentHealth -= 1.0f;
+
+	if (CurrentHealth <= 0.f)
+	{
+		Die();
+	}
+}
+
+void APSEnemy::PerformAttack()
+{
+	UE_LOG(PSEnemy, Display, TEXT("APSEnemy::PerformAttack()"))
+}
+
+void APSEnemy::Die()
+{
+	UE_LOG(PSEnemy, Display, TEXT("APSEnemy::Die()"));
+
+	bShouldMove = false;
+
+	if (DeathEffect)
+	{
+		DeathEffect->Activate(true);
+	}
+
+	float DestructionDelay = 2.0f; 
+	SetLifeSpan(DestructionDelay);
 }
 
 void APSEnemy::RevertSpriteColor()
--- Source/ProjectScale/Actors/PSEnemy.h
+++ Source/ProjectScale/Actors/PSEnemy.h
@@ -7,17 +7,21 @@
 #include "ProjectScale/Interfaces/PSCombatInterface.h"
 #include "PSEnemy.generated.h"
 
+DECLARE_LOG_CATEGORY_EXTERN(PSEnemy, Log, All);
+
 class UCapsuleComponent;
 class UPaperFlipbookComponent;
 class UMaterialInstanceDynamic;
+class UArrowComponent;
 
 UCLASS()
 class PROJECTSCALE_API APSEnemy : public AActor, public IPSCombatInterface
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	APSEnemy();
+	APSEnemy(FVector NewDirection);
 
 protected:
 	/* Overrides */
@@ -32,14 +36,42 @@ protected:
 	TObjectPtr<UCapsuleComponent> CapsuleComp;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<UPaperFlipbookComponent> FlipbookComp;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UArrowComponent> DirectionArrow;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UParticleSystemComponent> DeathEffect;
 
-public:
+	UFUNCTION(BlueprintCallable, Category = "Attack")
+	void PerformAttack();
 
+	/**/
+	UFUNCTION(BlueprintCallable)
+	void Die();
 
-private:
-
+	/**/
 	void RevertSpriteColor();
 
+	/**/
 	UPROPERTY()
 	TObjectPtr<UMaterialInstanceDynamic> DynamicMaterialInstance;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Health")
+	float MaxHealth{ 1.0f };
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Health")
+	float CurrentHealth;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Movement")
+	float MovementSpeed{ 100.0f };
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	FVector MovementDirection{ 1,0,0 };
+
+	UPROPERTY(EditDefaultsOnly, Category = "Attack")
+	float AttackDamage{ 1.0f };
+	UPROPERTY(EditDefaultsOnly, Category = "Attack")
+	float AttackRange { 5.0f };
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bShouldMove{ true };
+
 };
